4658a0b05dd9efc4feb86d81a80e04624b450298
BNURI <luckmon05@naver.com>
2019-06-29 03:30:31 +0900

    Day 12

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is somethin I love",
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 29489,
+    title: "Video super",
+    description: "This is somethin I love",
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 304858,
+    title: "Video nice",
+    description: "This is somethin I love",
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 3494950,
+    title: "Video perfect",
+    description: "This is somethin I love",
+    views: 24,
+    videoFile:
+      "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  }
+];

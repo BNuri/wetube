c01b48b9530550f5a5280a3aa573277fa3bcd331
BNURI <luckmon05@naver.com>
2019-07-02 02:21:43 +0900

    Day 15

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);

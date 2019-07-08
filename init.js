e8aacdccbfd71abb6a0c3c2f01d5fa3c2af8d41b
BNURI <luckmon05@naver.com>
2019-07-09 01:57:23 +0900

    day 22

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
+import "./db";
 import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT || 4000;
 

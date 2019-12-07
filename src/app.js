e349d3b100b638eb14513b1ce013a9494c2698cf
BNURI <luckmon05@naver.com>
2019-12-08 02:51:15 +0900

    to deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
+import path from "path";
 import MongoStore from "connect-mongo";
 import morgan from "morgan";
 import { localsMiddleware } from "./middlewares";
@@ -22,8 +23,8 @@ const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
